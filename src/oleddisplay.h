3a0013cd75e4f1d83ead3dcd9c3796688af9a8e3
weichinger <anton.weichinger@freenet.de>
2026-01-17 17:07:44 +0100

    first init

---
+++ src/oleddisplay.h
@@ -0,0 +1,102 @@
+#ifndef oleddisplay_h
+#define oleddisplay_h
+
+// #include "SSD1306.h"  // OLed SSD1306     4.3.0    for ESP by ThingPulse
+#include "SH1106.h" // OLed SH110X     2.1.10   for ESP by Adafruit
+
+// SSD1306  display(0x3c, 13, 12);
+SH1106 display(0x3c, 5, 4);
+enum eDisplayType
+{
+	STD,
+	ERR1,
+	ERR2
+};
+
+uint8_t uiPixOffset = 0;
+
+void oled_loeschen()
+{
+	display.clear();
+	display.display();
+}
+
+//__________________________________________________
+void initoled()
+{
+	display.init();
+	display.flipScreenVertically();
+	display.setFont(ArialMT_Plain_10);
+	display.setTextAlignment(TEXT_ALIGN_LEFT);
+	display.setContrast(0);
+}
+//-------------------------------------------------------------
+//                                Oled-Display
+//-------------------------------------------------------------
+void drawProgressBar(uint8_t uiOffset, int percent)
+{
+	// draw the progress bar
+	display.drawProgressBar(0, 53, 125, 10, percent);
+	// draw the percentage as String
+	display.setTextAlignment(TEXT_ALIGN_CENTER);
+	display.drawString(uiOffset + 64, 37, String(percent) + "%");
+	display.display();
+}
+
+//-------------
+// void displayinfo(eDisplayType dt) {
+//	switch (dt) {
+//	case STD:
+//		display.clear();
+//		display.setFont(ArialMT_Plain_10);
+//		display.setTextAlignment(TEXT_ALIGN_LEFT);
+//		char buf[120];
+//		sprintf(buf, "U: %i F: %.2f B: %i", uiNetzSpng, fNetzFreq, g_iBatterieLeistung);
+//		display.drawString(uiPixOffset, 5, buf);
+//		// zweite Zeile
+//		display.setFont(ArialMT_Plain_16);
+//		sprintf(buf, "V: %i  P: %i", g_uiVerbrauch, g_uiErzeugung);
+//		display.drawString(uiPixOffset, 20, buf);
+//
+//		drawProgressBar(uiPixOffset, g_uiBatterieLadezustand);
+//		display.display();
+//		uiPixOffset++;
+//		uiPixOffset = uiPixOffset % 8;
+//
+//		break;
+//	case ERR1:
+//		break;
+//	default:
+//		break;
+//	}
+//}
+//----------------------------------
+void oleddisplay(const char *c1, const char *c2, const char *c3, bool clear)
+{
+	if (clear)
+		display.clear();
+	display.setFont(ArialMT_Plain_10);
+	display.drawString(4, 10, c1);
+	display.drawString(4, 30, c2);
+	display.drawString(4, 50, c3);
+	display.display();
+}
+void oleddisplay(const char *c1, const char *c2, const char *c3)
+{
+	oleddisplay(c1, c2, c3, true);
+}
+void oleddisplay(const char *c1, const char *c2)
+{
+	oleddisplay(c1, c2, "", false);
+}
+
+//----------------------------------
+void oleddisplay(const char *c1)
+{
+	display.clear();
+	display.setFont(ArialMT_Plain_16);
+	display.drawString(4, 10, c1);
+	display.display();
+}
+
+#endif
